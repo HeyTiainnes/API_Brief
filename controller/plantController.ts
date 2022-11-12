f5745f0b148e33ebf38fdac340b18f786416725a
Tiainaittes <etienne.pomente@reseau.sncf.fr>
2022-11-12 12:24:41 +0100

    liaison route - controller, imports depuis express pour typage

--- controller/plantController.ts
+++ controller/plantController.ts
@@ -1,3 +1,13 @@
-class plantController{};
+import { getAllJSDocTags } from "typescript";
+import {Request, Response} from 'express';
+
+
+class plantController{
+
+getAll(req: Request, res: Response){
+
+
+}
+}
 
 export default plantController;
--- roads/plantRoad.ts
+++ roads/plantRoad.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import plantController from "../controller/plantController";
+
 
 const plantRoads = Router();
+const PlantController = new plantController(); // PlantController avec une majuscule, sinon conflit avec import plantController
 
 plantRoads.get('/', (req, res) => {});
 export default plantRoads;
